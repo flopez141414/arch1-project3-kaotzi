ea9140ffef912998b019118d32279afab3e8bad0
kaotzi <tjgeorge@miners.utep.edu>
2017-04-30 15:57:14 -0600

    modifying switch lib for 5 buttons

---
+++ p2swLib-mod/switchdemo.c
@@ -0,0 +1,25 @@
+#include <msp430.h>
+#include <sr.h>
+#include "p2switches.h"
+#define RED_LED BIT0
+#define GREEN_LED BIT6
+
+int
+main()
+{
+  p2sw_init(1);
+/*  or_sr(0x8);			 GIE (enable interrupts) */
+p1sw_init(1);
+  or_sr(0x8);			/* GIE (enable interrupts) */
+  
+  P1DIR |= RED_LED;
+  for(;;) {
+    P1OUT = (1 & p2sw_read());
+  }
+  
+  P1DIR |= GREEN_LED;
+  for(;;) {
+    P1OUT = (1 & p1sw_read());
+  }
+}
+
