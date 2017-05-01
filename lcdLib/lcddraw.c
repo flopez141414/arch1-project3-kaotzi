25f4c91792ef893c755a8df5fd51ce9c4d94de99
kaotzi <tjgeorge@miners.utep.edu>
2017-04-30 23:21:10 -0600

    switch rw & col on 8x12

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -106,8 +106,9 @@ void drawChar8x12(u_char rcol, u_char rrow, char c,
   u_char oc = c - 0x20;
 
   lcd_setArea(rcol, rrow, rcol + 7, rrow + 12); /* relative to requested col/row */
-  while (row < 13) {
-    while (col < 8) {
+  
+  while (col < 9) {
+    while (row < 12) {
       u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
       lcd_writeColor(colorBGR);
       col++;
