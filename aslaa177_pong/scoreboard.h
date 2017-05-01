099875e5e0e9df774ceaa89843a5726278394765
kaotzi <tjgeorge@miners.utep.edu>
2017-04-30 19:15:22 -0600

    cleaning up TI headers

---
+++ aslaa177_pong/scoreboard.h
@@ -0,0 +1,13 @@
+#ifndef ponggame_included
+#define scoreboard_included
+
+//extern unsigned char videomem[24*4];
+//extern unsigned int freq;
+//extern unsigned int sde;
+//extern unsigned volatile int play;
+
+void note( unsigned int fr );
+void scoreboard( int player );
+
+
+#endif // included
