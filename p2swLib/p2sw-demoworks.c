616a9fe2316666d4ad46aab27e2552f1f0d362f9
kaotzi <tjgeorge@miners.utep.edu>
2017-05-03 02:06:33 -0600

    testing

--- p2swLib/p2sw-demoworks.c
+++ p2swLib/p2sw-demoworks.c
@@ -7,8 +7,7 @@
 #include <lcddraw.h>
 #include <p2switches.h>
 #include <shape.h>
-//#include "shapedemo.c"
-abDrawPos(AbShape *shape, Vec2 *shapeCenter, u_int fg_color, u_int bg_color);
+#include shapedemo.
 
 AbRect recttop = {abRectGetBounds, abRectCheck, {screenWidth,2}}; 
 AbRect rectside ={abRectGetBounds, abRectCheck, {2, screenHeight}};
@@ -18,8 +17,6 @@ AbDiamond diamond = {abDiamondGetBounds, abDiamondCheck, 5};
 /** Initializes everything, clears the screen, draws "hello" and the circle */
 void main()
 {
-   
-    
   configureClocks();
   lcd_init();
   p2sw_init(15);
@@ -37,16 +34,5 @@ void main()
     str[4] = 0;
     drawString5x7(20,20, str, COLOR_GREEN, COLOR_BLUE);
    
-  }
-   Vec2 rectPost = {screenWidth/2, 3};
-   Vec2 rectPosl = { 3, screenHeight/2 };
-   Vec2 rectPosr = { screenWidth-3, screenHeight/2 };
-   Vec2 padPos = { screenWidth/2, screenHeight-3 };
-  abDrawPos((abShape*)&recttop, &rectPost, COLOR_BLACK, COLOR_BLUE);
-  abDrawPos((abShape*)&rectside, &rectPosl, COLOR_BLACK, COLOR_BLUE);
-  abDrawPos((abShape*)&rectside, &rectPosr, COLOR_BLACK, COLOR_BLUE);
-  abDrawPos((abShape*)&paddle, &padPos, COLOR_BLACK, COLOR_BLUE);
-  
-  
-
+  } 
 }
---
+++ shapeLib/shapedemoWorks.c
@@ -0,0 +1,72 @@
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "shape.h"
+
+const AbRect rect10 = {abRectGetBounds, abRectCheck, 60,10};; //makes horizontal banner across most of the screen
+const AbRect rect5 = {abRectGetBounds, abRectCheck, 4,16};; //makes horizontal banner across most of the screen
+AbTTile ttile = {abTTileGetBounds, abTTileCheck, 20}; //T tile
+AbITile itile = {abITileGetBounds, abITileCheck, 20}; //I tile
+AbLTile ltile = {abLTileGetBounds, abLTileCheck, 20}; //L tile
+
+void
+abDrawPos(AbShape *shape, Vec2 *shapeCenter, u_int fg_color, u_int bg_color)
+{
+  u_char row, col;
+  Region bounds;
+  abShapeGetBounds(shape, shapeCenter, &bounds);
+  lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1],
+	      bounds.botRight.axes[0]-1, bounds.botRight.axes[1]-1);
+  for (row = bounds.topLeft.axes[1]; row < bounds.botRight.axes[1]; row++) {
+    for (col = bounds.topLeft.axes[0]; col < bounds.botRight.axes[0]; col++) {
+      Vec2 pixelPos = {col, row};
+      int color = abShapeCheck(shape, shapeCenter, &pixelPos) ?
+	fg_color : bg_color;
+      lcd_writeColor(color);
+    }
+  }
+}
+
+
+
+int
+main()
+{
+  configureClocks();
+  lcd_init();
+  shapeInit();
+  Vec2 rectPos = screenCenter;
+  Vec2 rectPos2 = { 20, 50 };
+  Vec2 tPos = { 30, 20 };
+  Vec2 iPos = { 60, 50 };
+  Vec2 lPos = { 90, 20 };
+  Vec2 t2Pos = { 120, 50 };
+    
+  clearScreen(COLOR_BLUE);
+  drawString5x7(20,40, "Testing my Pong", COLOR_GREEN, COLOR_BLUE);
+  
+  drawString5x7(20,75, "Pong is Tested", COLOR_BLACK, COLOR_ORANGE);
+  shapeInit();
+  
+  abDrawPos((AbShape*)&rect5, &rectPos2, COLOR_YELLOW, COLOR_BLUE); // over to the left, covering part of first line of text
+  drawString5x7(5,150, "SW1up", COLOR_BLACK, COLOR_BLUE);
+  drawString5x7(15,140, "SW2-Strt", COLOR_BLACK, COLOR_BLUE);
+  drawString5x7(70,140, "SW3-bl", COLOR_BLACK, COLOR_BLUE);
+  drawString5x7(90,150, "SW4-dn", COLOR_BLACK, COLOR_BLUE);
+  drawString5x7(5,5,"Score: ", COLOR_WHITE, COLOR_BLUE);
+  
+  //testing TILT
+  shapeInit();
+  abDrawPos((AbShape*)&rect10, &rectPos, COLOR_ORANGE, COLOR_BLUE);
+  shapeInit();
+  abDrawPos((AbShape*)&ttile, &tPos, COLOR_RED, COLOR_BLUE);
+  shapeInit();
+  abDrawPos((AbShape*)&itile, &iPos, COLOR_YELLOW, COLOR_BLUE);
+  shapeInit();
+  abDrawPos((AbShape*)&ltile, &lPos, COLOR_WHITE, COLOR_BLUE);
+  shapeInit();
+  abDrawPos((AbShape*)&ttile, &t2Pos, COLOR_GREEN, COLOR_BLUE);
+  
+}
+
+
