77b85cc20e364476b0940d81e6f7381a6c0f0f2d
kaotzi <tjgeorge@miners.utep.edu>
2017-05-03 02:34:47 -0600

    shapes

---
+++ shapeLib/p2sw-demo.c
@@ -0,0 +1,53 @@
+/** \file lcddemo.c
+ *  \brief A simple demo that draws a string and circle
+ */
+
+#include <libTimer.h>
+#include <lcdutils.h>
+#include <lcddraw.h>
+#include <p2switches.h>
+#include <shape.h>
+//#include "shapedemo.c"
+abDrawPos(AbShape *shape, Vec2 *shapeCenter, u_int fg_color, u_int bg_color);
+
+AbRect recttop = {abRectGetBounds, abRectCheck, {screenWidth,2}}; 
+AbRect rectside ={abRectGetBounds, abRectCheck, {2, screenHeight}};
+AbRect paddle = {abRectGetBounds, abRectCheck, {screenWidth/5, 8}};
+AbDiamond diamond = {abDiamondGetBounds, abDiamondCheck, 5};
+
+/** Initializes everything, clears the screen, draws "hello" and the circle */
+void main()
+{
+   
+    
+  configureClocks();
+  lcd_init();
+  p2sw_init(15);
+  shapeInit();
+  or_sr(0x8);			/* GIE (enable interrupts) */
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLUE);
+   Vec2 rectPost = {screenWidth/2, 3};
+   Vec2 rectPosl = { 3, screenHeight/2 };
+   Vec2 rectPosr = { screenWidth-3, screenHeight/2 };
+   Vec2 padPos = { screenWidth/2, screenHeight-3 };
+  abDrawPos((AbShape*)&recttop, &rectPost, COLOR_BLACK, COLOR_BLUE);
+  abDrawPos((AbShape*)&rectside, &rectPosl, COLOR_BLACK, COLOR_BLUE);
+  abDrawPos((AbShape*)&rectside, &rectPosr, COLOR_BLACK, COLOR_BLUE);
+  abDrawPos((AbShape*)&paddle, &padPos, COLOR_BLACK, COLOR_BLUE);
+  //abDrawPos((AbShape*)&rect10, &rectPos, COLOR_ORANGE, COLOR_BLUE);
+  
+  drawString5x7(10,10, "switches:", COLOR_GREEN, COLOR_BLUE);
+  while (1) {
+    u_int switches = p2sw_read(), i;
+    char str[5];
+    for (i = 0; i < 4; i++)
+      str[i] = (switches & (1<<i)) ? '-' : '0'+i;
+    str[4] = 0;
+    drawString5x7(20,20, str, COLOR_GREEN, COLOR_BLUE);
+   
+  }
+   
+
+}
